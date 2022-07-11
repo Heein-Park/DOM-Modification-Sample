bab31d0a015683162f3934800ab71619149ebc4f
Heein Park <gmldls94@gmail.com>
2022-07-11 09:58:51 +0900

    Document Element 추가
    querySelector와 createElement를 이용해서 새 div를 추가

--- searchDOM.js
+++ searchDOM.js
@@ -1,4 +1,10 @@
 const onload = () => {
     const body = document.body;
     console.dir(body);
+
+    const container = body.querySelector('div#container');
+    
+    let newTweet = document.createElement('div');
+    container.appendChild(newTweet);
+
 };
