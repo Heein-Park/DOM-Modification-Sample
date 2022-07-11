1987e748beda1b861cc2bb5e40f7cdd95ca11a0f
Heein Park <gmldls94@gmail.com>
2022-07-11 10:30:38 +0900

    Tweet을 다 지우는 Doomsday 버튼 추가
    querySelectorAll을 통해 NodeList를 불러오고 iterator로 노드들을 동시에 삭제

--- searchDOM.js
+++ searchDOM.js
@@ -7,4 +7,16 @@ const onload = () => {
     newTweet.classList.add('tweet');
     container.appendChild(newTweet);
 
+    let ddButton = document.createElement('button');
+    ddButton.textContent = "Doomsday Button";
+    ddButton.classList.add('tweet', 'button');
+    container.appendChild(ddButton);
+
+    ddButton.addEventListener("click", () => {
+        console.log("Click");
+        let tweetList = body.querySelectorAll('div.tweet');
+        for(let tweet of tweetList) {
+            tweet.remove();
+        }
+    })
 };
